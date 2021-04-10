14c4f71f09405a1345c415a415b46cae0151f787
Georgy Petukhov <georgy.petukhov@quantifeed.com>
2021-04-10 17:03:33 +0800

    removing gatsby template stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Drag'a'Droppah`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Project K`,
+    description: `Project K`,
+    author: `Georgy Petukhov`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
