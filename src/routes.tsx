c7f4ccc10a6b6670cd071f00ee1d94c54872bac8
petukhov <petukhovg@gmail.com>
2024-04-30 00:54:40 +0800

    remove old code fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,5 @@
 import { isMobile } from 'react-device-detect';
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import MainPageOld from './old-code/MainPage';
 import ErrorPage from './pages/ErrorPage';
 import MainPage from './pages/MainPage';
 import TouchPage from './pages/TouchPage';
@@ -9,7 +8,6 @@ const router = createBrowserRouter(
     createRoutesFromElements(
         <>
             <Route path="/" element={isMobile ? <TouchPage /> : <MainPage />}></Route>
-            <Route path="/old" element={<MainPageOld />}></Route>
             <Route path="*" element={<ErrorPage />}></Route>
         </>,
     ),
